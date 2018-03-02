3c86e3267594cb204960b050d4b75e5a52500417
Daniel Nguyen <danngu@uw.edu>
2018-03-02 09:53:09 -0800

    Add daniel's stuff

--- server.R
+++ server.R
@@ -11,6 +11,7 @@ library(ggplot2)
 #Set working directory to source location and read in data file
 
 my.data <- read.csv('outbreaks.csv', stringsAsFactors=FALSE)
+View(my.data)
 
 # Create a server for app
 
@@ -98,6 +99,16 @@ my.server <- function(input, output) {
   
   
   # Daniel
+  filtered <- reactive({
+    sum.state <- group_by(my.data, State) %>%
+      filter(Month == input$month) %>%
+      mutate(Count = sum(Illnesses)) %>%
+      distinct(Year, .keep_all = TRUE) %>%
+      filter(Year == input$year) %>%
+      select(State, Count)
+    colnames(sum.state) <- c("State", "Record of Illnesses")
+    return(sum.state)
+  })
   
   
   
@@ -109,26 +120,12 @@ my.server <- function(input, output) {
   
   
   
+  output$message <- renderText({
+    return("hello")
+  })
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  output$text <- renderText({
-    return("HI")
+  output$table <- renderDataTable({
+    return(filtered())
   })
 }
 
--- ui.R
+++ ui.R
@@ -5,105 +5,97 @@ my.ui <- fluidPage(
   
   navbarPage("Foodborne Disease Outbreaks in the USA",
              
-     # Introduction    
+    # Introduction    
      
      tabPanel("Introduction"
         
      ),
-             
-    # Emma
-    tabPanel("Trends over Time",
-             
-      sidebarLayout(
-        sidebarPanel(
-          selectizeInput(
-          'id', label="Year", choices=NULL, multiple=F, selected="X2015",
-          options = list(create = TRUE,placeholder = 'Choose the year')
-          ),
-          # Make a list of checkboxes
-          radioButtons("radio", label = h3("Radio buttons"),
-          choices = list("Choice 1" = 1, "Choice 2" = 2)
-          )
-        ),
-        mainPanel(textOutput("text"))
-        )
-    ),
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # Daniel
-    
-    tabPanel("Seasonal Foods",
-       sidebarLayout(
-         sidebarPanel(
-           selectizeInput(
-             'id', label="Year", choices=NULL, multiple=F, selected="X2015",
-             options = list(create = TRUE,placeholder = 'Choose the year')
-           ),
-           # Make a list of checkboxes
-           radioButtons("radio", label = h3("Radio buttons"),
-                        choices = list("Choice 1" = 1, "Choice 2" = 2)
-           )
-         ),
-         mainPanel(textOutput("text")
-         )
-       )
-    ),
-    
-    
-    # Glorianne
-    
-    tabPanel("Most Common Ingredients",
-      sidebarLayout(
-         sidebarPanel(
-           selectizeInput(
-             'id', label="Year", choices=NULL, multiple=F, selected="X2015",
-             options = list(create = TRUE,placeholder = 'Choose the year')
-           ),
-           # Make a list of checkboxes
-           radioButtons("radio", label = h3("Radio buttons"),
-                        choices = list("Choice 1" = 1, "Choice 2" = 2)
-           )
-         ),
-         mainPanel(textOutput("text")
-         )
-       )
-    ),
-    
-    
-    
-    
-    
-    
-    
-    # Rahul
-    
-    
-    
-    
-    tabPanel("Advancement in Disease Treatments",    
-      sidebarLayout(
-        sidebarPanel(
-          selectizeInput(
+               
+      # Emma
+      tabPanel("Trends over Time",
+               
+        sidebarLayout(
+          sidebarPanel(
+            selectizeInput(
             'id', label="Year", choices=NULL, multiple=F, selected="X2015",
             options = list(create = TRUE,placeholder = 'Choose the year')
+            ),
+            # Make a list of checkboxes
+            radioButtons("radio", label = h3("Radio buttons"),
+            choices = list("Choice 1" = 1, "Choice 2" = 2)
+            )
           ),
-          # Make a list of checkboxes
-          radioButtons("radio", label = h3("Radio buttons"),
-                       choices = list("Choice 1" = 1, "Choice 2" = 2)
-          )
-        ),
-        mainPanel(textOutput("text")
+          mainPanel()
+        )
+      ),
+      
+
+      
+      
+      # Daniel
+      
+      tabPanel("Worst Months for Food Disease",
+         sidebarLayout(
+           sidebarPanel(
+             textOutput("text"), 
+             sliderInput("year",
+                         "Which Year?",
+                         value = 2006,
+                         min = 1998,
+                         max = 2015, sep = ""),
+             selectInput("month", "Which Month?", choices = c("January", "February", "March", "April", "May", "June", "July", "August",
+                                                     "September", "October", "November", "December"), selected = "January", multiple = FALSE)
+           ),
+           mainPanel( tabsetPanel(type = "tabs",
+                                  tabPanel("Map"),
+                                  tabPanel("Table", br(), dataTableOutput("table")))
+            )
+         )
+      ),
+      
+      
+      # Glorianne
+      
+      tabPanel("Most Common Ingredients",
+        sidebarLayout(
+           sidebarPanel(
+             selectizeInput(
+               'id', label="Year", choices=NULL, multiple=F, selected="X2015",
+               options = list(create = TRUE,placeholder = 'Choose the year')
+             ),
+             # Make a list of checkboxes
+             radioButtons("radio", label = h3("Radio buttons"),
+                          choices = list("Choice 1" = 1, "Choice 2" = 2)
+             )
+           ),
+           mainPanel()
+         )
+      ),
+      
+      
+      
+      
+      # Rahul
+      
+      
+      
+      
+      tabPanel("Advancement in Disease Treatments",    
+        sidebarLayout(
+          sidebarPanel(
+            selectizeInput(
+              'id', label="Year", choices=NULL, multiple=F, selected="X2015",
+              options = list(create = TRUE,placeholder = 'Choose the year')
+            ),
+            # Make a list of checkboxes
+            radioButtons("radio", label = h3("Radio buttons"),
+                         choices = list("Choice 1" = 1, "Choice 2" = 2)
+            )
+          ),
+          mainPanel( )
         )
       )
-    )
+  
   )
 )
 
