e2bb4d3bf1c4783f7d03c37959100fe50ad77b84
rwar <chying@uw.edu>
2018-03-07 07:48:04 -0800

    Added graphs and fixed UI

--- server.R
+++ server.R
@@ -17,61 +17,70 @@ my.data <- read.csv('outbreaks.csv', stringsAsFactors=FALSE)
 # Create a server for app
 
 my.server <- function(input, output) {
-  # Gloriane
- # getYear <- reactive({
-
-  #  data.set <- my.data["state" != "Multistate"]
-   # data.set <- na.omit(data.set) %>%
-  #  filter(Year == input$year) # CHANGE "YEAR" 
-  #  return(data.set)
-    
-  #})
   
-#  filterFood <- reactive({# Most mentioned Ingredients #Most mentioned species/Illness # Is it differnt each year?
-   
- #   data.set <- getYear() %>%
-  #    select(Year, Ingredient, Food, Species, State)
+ 
+
+    data.set <- my.data["state" != "Multistate"]
+    data.set <- na.omit(data.set) %>%
+      filter(Food != "" & Location != "")
     
-   # # Distinct specific ingridients in that YEAR
-    #list.of.ingredients <- distinct(data.set, Ingredient)
-    #list.of.species <- distinct(data.set, Species)
+  getYear <- reactive({
+    print(input$yearinput)
+    print(is.numeric(input$yearinput))
+    
+    data.set <- data.set %>%
+      filter(Year == input$yearinput ) # CHANGE "YEAR" 
+    return(data.set)
+    
+  })
+  
+  
+
+
+  
+  output$food <- renderPlot({
+    
+    list.of.ingredients <- getYear() %>%
+    select(Year, Food)
     
     # Cleaning up names preparing for manipulation
-    #food.list <- data.frame("Food" = unlist(strsplit(data.set$Food, ";")), stringsAsFactors = FALSE)
-    #food.list <- data.frame("Food" = unlist(strsplit(food.list$Food, ",")), stringsAsFactors = FALSE)
-  #  food.list <- as.data.frame(lapply(food.list, trimws), stringsAsFactors = FALSE)
+    food.list <- data.frame("Food" = unlist(strsplit(list.of.ingredients$Food, ";")), stringsAsFactors = FALSE)
+    food.list <- data.frame("Food" = unlist(strsplit(food.list$Food, ",")), stringsAsFactors = FALSE)
+    food.list <- as.data.frame(lapply(food.list, trimws), stringsAsFactors = FALSE)
     
-   # food.list <- food.list %>%
-  #    count(Food, sort = TRUE) %>%
-  #    filter(Food != "Unspecified" & Food != "Other") %>%
-   #   group_by(Food) %>%
-  #    arrange(desc(n))
-   # return(food.list)
-  #})
-  
-#  filterLocation <- reactive({
- #   data.set <- getYear() %>%
-  #  location.list <- data.set  %>% select(Location)
-   # location.list <- data.frame("Location" = unlist(strsplit(location.list$Location, ";")), stringsAsFactors = FALSE)
-    #location.list <- as.data.frame(lapply(location.list, trimws), stringsAsFactors = FALSE)
-    #location.list <- location.list %>%
-    #  filter(Location != "Unknown") %>%
-    #  count(Location, sort = TRUE) %>%
-    #  group_by(Location) %>%
-    #  summarize("n" = sum(n)) %>%
-    #  arrange(desc(n))
-  #  return(location.list)
-  #})
-  
-  #output$food <- renderPlot({
-   #  ggplot(data = filterFood()) +
-   #     geom_bar(mapping = aes(x = food.list[1:10], y = n[1:10], fill = Location), stat = "count")
-  #})
+    food.list <- food.list %>%
+      count(Food, sort = TRUE) %>%
+      filter(Food != "Unspecified" & Food != "Other") %>%
+      group_by(Food) %>%
+      arrange(desc(n))
+    food.list <- food.list[1:10, ]
+    
+    p <- ggplot(data = food.list) +
+       geom_bar(mapping = aes(x = Food, y = n, fill = Food), stat = "identity") +
+      labs(title= (paste("Top contaminated food in", input$year)))
+    
+    return(p)
+  })
 
- # output$location <- renderPlot({
- #   ggplot(data = my.data) +
-#      geom_point(mapping = aes(x=Year, y = Illnesses))
- # })
+  output$location <- renderPlot({
+      location.list <- getYear() %>%
+        select(Location)
+      location.list <- data.frame("Location" = unlist(strsplit(location.list$Location, ";")), stringsAsFactors = FALSE)
+      location.list <- as.data.frame(lapply(location.list, trimws), stringsAsFactors = FALSE)
+      location.list <- location.list %>%
+        filter(Location != "Unknown") %>%
+        count(Location, sort = TRUE) %>%
+        group_by(Location) %>%
+        summarize("n" = sum(n)) %>%
+        arrange(desc(n))
+      
+    p <- ggplot(data = location.list) +
+      geom_bar(mapping = aes(x = Location, y = n, fill = Location), stat = "identity") +
+      coord_flip() +
+      labs(title = paste("Number of recorded contamination based on Location in", input$year))
+
+    return(p)
+  })
   
   
   output$text <- renderText({
--- ui.R
+++ ui.R
@@ -11,23 +11,22 @@ my.ui <- fluidPage(
         
      ),
              
-    # Emma
-    tabPanel("Trends over Time",
-             
-      sidebarLayout(
-        sidebarPanel(
-          selectizeInput(
-          'id', label="Year", choices=NULL, multiple=F, selected="X2015",
-          options = list(create = TRUE,placeholder = 'Choose the year')
-          ),
-          # Make a list of checkboxes
-          radioButtons("radio", label = h3("Radio buttons"),
-          choices = list("Choice 1" = 1, "Choice 2" = 2)
-          )
-        ),
-        mainPanel(textOutput("text"))
-        )
-    ),
+     # Emma
+     tabPanel("Trends over Time",
+              
+              sidebarLayout(
+                sidebarPanel(
+                  sliderInput('year', label='Year', min=1998, max=2015,
+                              value=(c(1998,2015)), sep = ""),
+                  # Make a list of checkboxes
+                  selectInput('type', label='Choose Illnesses or Hospitalization', 
+                              choices = c('Illnesses', 'Hospitalization'))
+                ),
+                mainPanel(
+                  plotOutput('map_plot')
+                )
+              )
+     ),
     
     
     
@@ -37,25 +36,32 @@ my.ui <- fluidPage(
     
     
     
-    
-    # Daniel
-    
-    tabPanel("Seasonal Foods",
-       sidebarLayout(
-         sidebarPanel(
-           selectizeInput(
-             'id', label="Year", choices=NULL, multiple=F, selected="X2015",
-             options = list(create = TRUE,placeholder = 'Choose the year')
-           ),
-           # Make a list of checkboxes
-           radioButtons("radio", label = h3("Radio buttons"),
-                        choices = list("Choice 1" = 1, "Choice 2" = 2)
-           )
-         ),
-         mainPanel(textOutput("text")
-         )
-       )
-    ),
+     # Daniel
+     
+     tabPanel("Worst Diseases",
+              sidebarLayout(
+                sidebarPanel(
+                  textOutput("text"), 
+                  sliderInput("year",
+                              "Which Year?",
+                              value = 2006,
+                              min = 1998,
+                              max = 2015, sep = "", animate = TRUE),
+                  radioButtons("sort", label = "Get Table of",
+                               choices = list('10 Most Recorded Diseases', '10 Least Recorded Diseases', 'All Recorded Diseases')
+                  )
+                ),
+                mainPanel( tabsetPanel(type = "tabs",
+                                       tabPanel("Worst Disease", tags$h3(textOutput("header")), br(), plotOutput("plot"), br(),  
+                                                verbatimTextOutput("hover"), br(), tags$h4(textOutput("head")),textOutput("message"), br(),
+                                                tags$h4(textOutput("head2")),
+                                                textOutput("message2"), br(),tags$h4(textOutput("head3")), textOutput("message3")),
+                                       tabPanel("Diseases", tags$h3(textOutput("header1")), br(), plotOutput("plot2"), textOutput("note"), 
+                                                br(), dataTableOutput("table")), 
+                                       tabPanel("Analysis & Thoughts", tags$h3(textOutput("head4")), tags$h4(textOutput("head5")), textOutput("message5")))
+                )
+              )
+     ),
     
     
     # Gloriane
@@ -64,11 +70,10 @@ my.ui <- fluidPage(
       sidebarLayout(
          sidebarPanel(
            sliderInput(
-             'year', label="Year", value = 2015, min = 1998, max = 2015
+             'yearinput', label="Year", value = 2015, min = 1998, max = 2015
            )
          ),
          mainPanel(
-           textInput('text', label = "Write"),
            plotOutput("food"),
            plotOutput("location")
          )
@@ -87,24 +92,22 @@ my.ui <- fluidPage(
     
     
     tabPanel("Advancement in Disease Treatments",    
-      sidebarLayout(
-        sidebarPanel(
-          selectizeInput(
-            'id', label="Year", choices=NULL, multiple=F, selected="X2015",
-            options = list(create = TRUE,placeholder = 'Choose the year')
-          ),
-          # Make a list of checkboxes
-          radioButtons("radio", label = h3("Radio buttons"),
-                       choices = list("Choice 1" = 1, "Choice 2" = 2)
-          )
-        ),
-        mainPanel(textOutput("text")
-        )
-      )
+             sidebarLayout(
+               sidebarPanel(
+                 selectizeInput(
+                   'id', label="Year", choices=NULL, multiple=F, selected="X2015",
+                   options = list(create = TRUE,placeholder = 'Choose the year')
+                 ),
+                 # Make a list of checkboxes
+                 radioButtons("radio", label = h3("Radio buttons"),
+                              choices = list("Choice 1" = 1, "Choice 2" = 2)
+                 )
+               ),
+               mainPanel( )
+             )
     )
+    
   )
 )
-
-
 
 shinyUI(my.ui)
